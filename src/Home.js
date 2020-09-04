9b655aa32cf55f584b4be31e036aa88fed9f7adc
ysmith329 <62859075+ysmith329@users.noreply.github.com>
2020-09-03 23:00:54 -0400

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Home = () => (
+    <div>
+        <h2>Train. Certify. Succeed</h2>
+        <p>You can develop your individual brand through ATG Learning training options that will help you get pay raises, promotions, and professional satisfaction. We have the largest course portfolio available, decades of experience, trained instructors, and the commitment to ensure your success.</p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </div>
+)
